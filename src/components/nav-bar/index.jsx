e3a266719fca3d6e6706d695a7e2323cd3988c25
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2023-04-05 00:35:58 +0100

    [feature] - update nav bar icons

--- src/components/nav-bar/index.jsx
+++ src/components/nav-bar/index.jsx
@@ -1,20 +1,51 @@
 import { Link } from "react-router-dom";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import { Box, AppBar, Toolbar, Button, IconButton, Icon } from "@mui/material";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isLogedIn, setLogedIn] = useState(false);
+
+  const onHandleLogin = () => {
+    setLogedIn(!isLogedIn);
+  };
+
   return (
-    <nav>
-      <p>
-        <Link to="/">Bakery Store</Link>
-      </p>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/cart">Cart</Link>
-        </li>
-      </ul>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "transparent",
+        }}>
+        <Toolbar
+          sx={{
+            justifyContent: "flex-end",
+          }}>
+          <IconButton>
+            <Link to="/">{<HomeOutlinedIcon />}</Link>
+          </IconButton>
+          <IconButton>
+            <Link to="/cart">{<ShoppingCartOutlinedIcon />}</Link>
+          </IconButton>
+          {isLogedIn ? (
+            <IconButton onClick={onHandleLogin}>
+              <AccountCircleOutlinedIcon />
+            </IconButton>
+          ) : (
+            <Button
+              sx={{
+                color: "black",
+              }}
+              color="inherit"
+              onClick={onHandleLogin}>
+              Login
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
