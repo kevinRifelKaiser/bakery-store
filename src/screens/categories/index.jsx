d031ce5f684b2d49399b7249bed88df2104c1ce2
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2023-04-06 00:26:08 +0100

    [feature] - create products screen and data. Add and configure rome to this proyect

--- src/screens/categories/index.jsx
+++ src/screens/categories/index.jsx
@@ -1,22 +1,22 @@
-import { CategoryCard } from "../../components/index";
 import { Box, Grid } from "@mui/material";
+import { CategoryCard } from "../../components/index";
 import { CATEGORIES } from "../../constants/data/index";
 
 const Categories = () => {
-  return (
-    <>
-      <h1>Categories</h1>
-      <Box>
-        <Grid container spacing={3}>
-          {CATEGORIES.map((element) => (
-            <Grid key={element.id} item xs={12} md={6} lg={3}>
-              <CategoryCard imgURL={element.imgURL} title={element.title} />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    </>
-  );
+	return (
+		<>
+			<h1>Categories</h1>
+			<Box>
+				<Grid container spacing={3}>
+					{CATEGORIES.map((element) => (
+						<Grid key={element.id} item xs={12} md={6} lg={3}>
+							<CategoryCard imgURL={element.imgURL} title={element.title} />
+						</Grid>
+					))}
+				</Grid>
+			</Box>
+		</>
+	);
 };
 
 export default Categories;
--- src/screens/index.js
+++ src/screens/index.js
@@ -2,3 +2,4 @@ export { default as HomePage } from "./home/index";
 export { default as Categories } from "./categories/index";
 export { default as Cart } from "./cart/index";
 export { default as NotFound } from "./not-found/index";
+export { default as ProductsPage } from "./products/index";
